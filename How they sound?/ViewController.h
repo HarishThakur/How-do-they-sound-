214fbcf24a12f726e4264edb281cff89d9aad502
Harish Singh <harishsingh@Harish-iMac.local>
2015-09-28 16:44:43 +0530

    Initial Commit

---
+++ How they sound?/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  How they sound?
+//
+//  Created by Harish Singh on 28/09/15.
+//  Copyright (c) 2015 Sourcebits Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ How they sound?/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  How they sound?
+//
+//  Created by Harish Singh on 28/09/15.
+//  Copyright (c) 2015 Sourcebits Technologies. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
